8621c8f4c8b64303fba79dd36984f597fad7fc4e
Rafael Sousa <guilhermefeller2002@gmail.com>
2023-07-04 08:44:57 -0300

    feature: filter

--- src/app/modules/doc-helper/pages/scenarios/scenarios.component.ts
+++ src/app/modules/doc-helper/pages/scenarios/scenarios.component.ts
@@ -1,6 +1,14 @@
 import { Component, inject } from '@angular/core';
 import { toSignal } from '@angular/core/rxjs-interop';
-import { delay, map } from 'rxjs';
+import { FormBuilder } from '@angular/forms';
+import {
+  delay,
+  distinctUntilChanged,
+  map,
+  startWith,
+  switchMap,
+  tap,
+} from 'rxjs';
 import { ScenariosService } from 'src/app/core/services/scenarios/scenarios.service';
 
 @Component({
@@ -10,6 +18,14 @@ import { ScenariosService } from 'src/app/core/services/scenarios/scenarios.serv
 })
 export class ScenariosComponent {
   scenariosService = inject(ScenariosService);
+  formBuilder = inject(FormBuilder);
+
+  scenarioFilterForm = this.formBuilder.group({
+    cardNumber: [''],
+    sprint: [''],
+    cardHolder: [''],
+    cardReviewer: [''],
+  });
 
   scenarios$ = this.scenariosService.getAllScenarios().pipe(
     map((scenarios) => {
@@ -22,5 +38,48 @@ export class ScenariosComponent {
     })
   );
 
-  scenarios = toSignal(this.scenarios$);
+  filteredScenarios$ = this.scenarioFilterForm.valueChanges.pipe(
+    startWith(this.scenarioFilterForm.value),
+    distinctUntilChanged(),
+    switchMap((formValue) => {
+      return this.scenarios$.pipe(
+        map((scenarios) => {
+          return scenarios.filter((scenario: any) => {
+            const cardNumber = scenario.cardNumber
+              ? scenario.cardNumber.toLowerCase()
+              : '';
+            const sprint = scenario.sprint ? scenario.sprint.toLowerCase() : '';
+            const cardHolder = scenario.cardHolder
+              ? scenario.cardHolder.toLowerCase()
+              : '';
+            const cardReviewer = scenario.cardReviewer
+              ? scenario.cardReviewer.toLowerCase()
+              : '';
+
+            const filterCardNumber = formValue.cardNumber
+              ? formValue.cardNumber.toLowerCase()
+              : '';
+            const filterSprint = formValue.sprint
+              ? formValue.sprint.toLowerCase()
+              : '';
+            const filterCardHolder = formValue.cardHolder
+              ? formValue.cardHolder.toLowerCase()
+              : '';
+            const filterCardReviewer = formValue.cardReviewer
+              ? formValue.cardReviewer.toLowerCase()
+              : '';
+
+            return (
+              cardNumber.includes(filterCardNumber) &&
+              sprint.includes(filterSprint) &&
+              cardHolder.includes(filterCardHolder) &&
+              cardReviewer.includes(filterCardReviewer)
+            );
+          });
+        })
+      );
+    })
+  );
+
+  scenarios = toSignal(this.filteredScenarios$);
 }
